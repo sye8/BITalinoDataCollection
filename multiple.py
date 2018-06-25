319c7082a5b2fd08c4abd646f393d43b3251ab26
Sifan Ye <sye8@u.rochester.edu>
2018-06-25 16:31:08 +0200

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -12,7 +12,6 @@ import numpy as np
 if sys.version_info[0] < 3:
     from bitalino import BITalino
 else:
-    print("Python Version 3: Using bitalino3X.py")
     from bitalino3X import BITalino
 
 import dataCollection as dc
